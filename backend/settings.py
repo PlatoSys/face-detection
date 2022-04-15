ea0ee505a55e5718358fac71bc4367e8bed28cc3
Platoni <platoni.injgia@flatrocktech.com>
2022-04-15 11:33:02 +0400

    Update db settings

--- backend/settings.py
+++ backend/settings.py
@@ -104,11 +104,11 @@ CORS_ALLOWED_ORIGINS = [
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "face_detection",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",
-        "PORT": "5432",
+        "NAME": os.environ.get('DB_NAME'),
+        "USER": os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('DB_PASSWORD'),
+        "HOST": os.environ.get('DB_HOST'),
+        "PORT": 5432,
     }
 }
 
