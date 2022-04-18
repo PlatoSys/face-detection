d00e63fb36775aeb96ede1ec72499759893a400c
PlatoSys <platoniinjgia@gmail.com>
2022-04-18 19:36:47 +0400

    Fix colors in detection

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Builds nacessary images.
 ### `docker-compose up`
 
 Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view frontend it in your browser.
+Open [http://localhost:3000](http://localhost:3000) to view frontend it in your browser.\
 Open [http://localhost:8000](http://localhost:8000) to view backend it in your browser.
 
 The page will reload when you make changes.
--- api/machine_learning/face_detection.py
+++ api/machine_learning/face_detection.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import cv2
 from skimage import io
 from keras.models import load_model
@@ -20,16 +21,19 @@ class Detection:
         self.faces = detector.detect_faces(self.frame)
         self.save_path = save_path
         self.detect_eyes = detect_eyes
+        self.rectangle_colors = {}
 
     def detect_faces(self):
-        self._add_bounding_boxes()
+        self.faces = [face for face in self.faces if face['confidence'] > 0.85]
         self.predict_age()
+        self._add_bounding_boxes()
 
     def _add_bounding_boxes(self):
         for result in self.faces:
             x, y, w, h = result['box']
             x1, y1 = x + w, y + h
-            cv2.rectangle(self.frame, (x, y), (x1, y1), (0, 0, 255), 2)
+            color = self.rectangle_colors[f'{x}_{y}_{w}_{h}'].value
+            cv2.rectangle(self.frame, (x, y), (x1, y1), color, 2)
             if self.detect_eyes:
                 self._add_bounding_circles(result['keypoints'])
 
@@ -40,21 +44,21 @@ class Detection:
         cv2.circle(self.frame, (keypoints['mouth_left']), 2, (0, 155, 255), 2)
         cv2.circle(self.frame, (keypoints['mouth_right']), 2, (0, 155, 255), 2)
 
-    def _put_text(self, text, x, y, scale=0.5):
+    def _put_text(self, text, x, y, scale, color):
         font = cv2.FONT_HERSHEY_SIMPLEX
 
         cv2.putText(self.frame, text, (x, y - 10),
-                    font, scale, (0, 0, 255), 1, 2)
+                    font, scale, color.value, 2, 2)
 
     def predict_age(self):
         MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
         GENDER_MODEL = './api/machine_learning/weights/deploy_gender.prototxt'
         GENDER_PROTO = './api/machine_learning/weights/gender_net.caffemodel'
-        MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
         GENDER_LIST = ['Male', 'Female']
 
         gender_net = cv2.dnn.readNetFromCaffe(GENDER_MODEL, GENDER_PROTO)
 
+        width = self.frame.shape[0]
         for face in self.faces:
             x, y, w, h = face['box']
             x_offset = int(w * 0.5)
@@ -79,8 +83,12 @@ class Detection:
             gender = GENDER_LIST[index]
             confidence_score = gender_preds[0][index]
 
+            scale = (w / width if float(w / width) > 0.3 else 0.3) + 0.2
+            color = Color.BLUE if gender == 'Male' else Color.RED
+            color = color if confidence_score > 0.8 else Color.GREEN
+            self.rectangle_colors[f'{x}_{y}_{w}_{h}'] = color
             self._put_text(f'{gender}-{round(confidence_score*100, 1)}%',
-                           x=x, y=y, scale=1)
+                           x=x, y=y, scale=scale, color=color)
 
     def predict_emotion(self):
         pass
@@ -115,3 +123,10 @@ class Detection:
 
     def save(self):
         cv2.imwrite(self.save_path, self.frame)
+
+
+class Color(Enum):
+
+    RED = (0, 0, 255)
+    GREEN = (0, 255, 0)
+    BLUE = (255, 0, 0)
